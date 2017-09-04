f911568ec72abf06e76ded4bbae044e7f425ac67
Oliver Caine <olliecaine@gmail.com>
2017-09-04 17:19:10 +1000

    Updating deploy to Heroku script

--- bash/Create Heroku remote for this branch.sh
+++ bash/Create Heroku remote for this branch.sh
@@ -4,12 +4,17 @@ cd "$(dirname "$0")"
 cd ../
 ${0} # run current file
 
+appName="starterproject"
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 echo "Creating remote on Heroku"
-heroku create --remote $branch olivercainev2-$branch --region eu
+heroku create --remote $branch $appName-$branch --region eu
 
 echo "Opening app..."
-heroku open -a olivercainev2-$branch
+heroku open -a $appName-$branch
+
+echo "Pushing empty commit to trigger deploy"
+git commit --allow-empty -m "Commit to automatically trigger deploy for first time"
+git push
 
 echo "Complete"
