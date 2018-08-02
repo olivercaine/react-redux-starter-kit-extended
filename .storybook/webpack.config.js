87efe5ba695985471a89a712229d684012ad50b5
Oliver Caine <olliecaine@gmail.com>
2018-08-02 21:35:08 +1000

    Fixing Storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,8 +2,20 @@ const path = require('path');
 const webpackConfig = require('../build/webpack.config')
 
 module.exports = {
+  resolve: {
+    modules: [
+      'node_modules',
+    ],
+    extensions: ['*', '.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
 	module: {
 		rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'awesome-typescript-loader'],
+        include: path.resolve(__dirname, '../')
+      },
 			{
 				test: /\.(png|jpg|gif)$/,
 				loader: 'url-loader',
