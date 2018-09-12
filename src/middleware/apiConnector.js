c0af2f9d1be1d367d3857e028abc25b8af818cd0
Oliver Caine <olliecaine@gmail.com>
2018-09-12 11:03:05 +1000

    Improving example middleware

--- src/middleware/apiConnector.js
+++ src/middleware/apiConnector.js
@@ -1,6 +1,9 @@
-import { COUNTER_DOUBLE_ASYNC } from '../routes/Counter/modules/counter.js'
+import { increment, COUNTER_INCREMENT } from '../routes/Counter/modules/counter.js'
 import { fetchAsync } from '../functions'
 
+/*
+ Recieves all actions but only processes specifc ones
+*/
 export default store => {
   let state = store.getState()
   return next => {
@@ -9,10 +12,11 @@ export default store => {
       console.log('next state', store.getState())
       let result = next(action)
       switch (action.type) {
-        case COUNTER_DOUBLE_ASYNC:
+        case COUNTER_INCREMENT:
           fetchAsync('http://www.fakeresponse.com/api/?sleep=2')
             .then((data) => {
               console.log(data)
+              store.dispatch(increment(100))
             })
           break
         default:
