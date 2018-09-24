bdf1f770bbe8ea70f6b31af5c34c955257aaaeff
Oliver Caine <olliecaine@gmail.com>
2018-09-25 01:55:04 +1000

    Fixing lint

--- src/middleware/ApiConnector.ts
+++ src/middleware/ApiConnector.ts
@@ -1,21 +1,18 @@
-import { increment, COUNTER_INCREMENT } from '../routes/Counter/modules/counter'
 import { fetchAsync } from '../Functions'
+import { COUNTER_INCREMENT, increment } from '../routes/Counter/modules/counter'
 
 /*
   Receives all actions but only processes specific ones
 */
-export default store => {
-  let state = store.getState()
-  return next => {
-    return action => {
-      console.log('Dispatching: ', action)
-      console.log('Next state: ', store.getState())
-      let result = next(action)
+export default (store) => {
+  const state = store.getState()
+  return (next) => {
+    return (action) => {
+      const result = next(action)
       switch (action.type) {
         case COUNTER_INCREMENT:
           fetchAsync('http://www.fakeresponse.com/api/?sleep=2')
             .then((data) => {
-              console.log(data)
               store.dispatch(increment(100))
             })
           break
