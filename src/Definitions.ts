a55d7bd06b647e568198e2011f425543a7ba5601
Oliver Caine <olliecaine@gmail.com>
2018-09-24 19:41:00 +1000

    Recommendation comment

--- src/Definitions.ts
+++ src/Definitions.ts
@@ -1,3 +1,6 @@
+// As definitions are likely to change during the early stages of development it's recommended to keep all
+// all definitions - models, interfaces etc - into this file until it gets too big to manage.
+
 export interface RootState {
   counter: number;
 }
