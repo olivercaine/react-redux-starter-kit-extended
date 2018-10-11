6cb6fc4f45996f0933b6df60446b04a769489cdd
Oliver Caine <olliecaine@gmail.com>
2018-10-11 11:59:15 +1100

    Adding Utils folder

---
+++ src/utils/date.ts
@@ -0,0 +1,5 @@
+export const calculateAge = (birthday: Date): number => {
+  const ageDifMs = Date.now() - birthday.getTime();
+  const ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
