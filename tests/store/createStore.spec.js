b18329954e2aab2a44e95ac421419241cb68a8fd
Oliver Caine <olliecaine@gmail.com>
2019-02-28 01:47:35 +1100

    TEst

--- tests/store/createStore.spec.js
+++ tests/store/createStore.spec.js
@@ -14,6 +14,11 @@ describe('(Store) createStore', () => {
     expect(store.asyncReducers).to.be.empty()
   })
 
+  it('DELETE-ME', () => {
+    expect(store.asyncReducers).to.be.an('object')
+    expect(store.asyncReducers).to.be.empty()
+  })
+
   describe('(Location)', () => {
     it('store should be initialized with Location state', () => {
       const location = {
