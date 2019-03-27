566293bdd16d383d0d789e575852b74f6813625e
Oliver Caine <olliecaine@gmail.com>
2019-03-27 18:56:04 +1100

    Changing month

--- src/utils/String.ts
+++ src/utils/String.ts
@@ -3,5 +3,5 @@ import { Months } from 'month-enum-ts/lib/months';
  * This is just an example.
  */
 export function sayWelcome() {
-    return `TypeScript says the month is ${Months[new Date().getMonth()]}`;
+    return `TypeScript says the month is ${Months[new Date().getMonth() + 3]}`;
 }
