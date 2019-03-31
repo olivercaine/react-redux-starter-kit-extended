0abcd8d7f84bf57d1892e6b54d6e0609bea334f6
Oliver Caine <olliecaine@gmail.com>
2019-03-31 21:02:28 +1100

    Bumping common lib

--- src/utils/String.ts
+++ src/utils/String.ts
@@ -1,7 +1,7 @@
-import { Months } from 'month-enum-ts/lib/Months';
+import { Months } from '@common/month-enum-ts/lib/Months';
 /**
  * This is just an example.
  */
 export function sayWelcome() {
-    return `TypeScript says the month is ${Months[new Date().getMonth() + 3]}`;
+    return `TypeScript says the month is ${Months[new Date().getMonth()]}`;
 }
