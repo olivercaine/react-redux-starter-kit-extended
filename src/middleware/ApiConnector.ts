50c1483788d169a73ce48086af4ac6598cdf9ed2
Oliver Caine <olliecaine@gmail.com>
2019-04-23 01:03:47 +1000

    Typing middleware

--- src/middleware/ApiConnector.ts
+++ src/middleware/ApiConnector.ts
@@ -1,8 +1,9 @@
 import { Defaults } from 'Constants'
+import { Middleware } from 'redux';
 import { COUNTER_DOUBLE_ASYNC, increment } from '../routes/Counter/modules/counter'
 
 // Receives all actions but only processes ones defined below before they reach the store's reducer.
-export const apiConnector = (store) => (next) => (action) => {
+export const apiConnector: Middleware = (store) => (next) => (action) => {
 
   switch (action.type) {
     case COUNTER_DOUBLE_ASYNC:
@@ -12,5 +13,5 @@ export const apiConnector = (store) => (next) => (action) => {
       break
   }
 
-  next(action);
+  return next(action);
 }
