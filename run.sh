71ccf280254113ff3a37b61823d079c5ad4dd10a
Oliver Caine <olliecaine@gmail.com>
2020-02-05 11:00:02 +1100

    Improving docker commands

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Build base image (Node alpine)
 docker build . -f ./devops/Dockerfile.base -t olliecaine/base:master 
 
+# Extend base image by adding dev dependencies (e.g. Chrome)
 docker build . -f ./devops/Dockerfile.dev -t olliecaine/dev:master
 
-docker build . --target stage-build -t olliecaine/client:master
+# if("dev" flag, e.g. ./run.sh dev){
+    # Run app in dev environment
+    docker run --rm -v $(pwd):/project -p 3000:3000 $(docker build . --target stage-develop -q)
+# } else {
+    # Build app in dev environment
+    # docker build . --target stage-build -t olliecaine/client:master
+# }
