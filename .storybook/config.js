9591c6a90d840fac73429ebf6e018d19a0f2114d
Oliver Caine <olliecaine@gmail.com>
2020-11-09 01:22:45 +1100

    Moving Storybook stories to be next to their components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/react';
 
+// automatically import all files ending in *.stories.tsx
+const req = require.context('../src', true, /\.stories.tsx$/);
 function loadStories() {
-  require('.');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
---
+++ src/routes/Home/components/HomeView.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import HomeView from './HomeView'
+
+storiesOf('HomeView', module)
+    .add('default', () => <HomeView />)
