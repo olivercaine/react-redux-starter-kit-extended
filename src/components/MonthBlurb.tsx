1875c66bf37ccb048bc3313ec95bf1b049016c36
Oliver Caine <olliecaine@gmail.com>
2021-01-22 11:38:39 +1100

    Fixing lint:fix command

--- src/components/MonthBlurb.tsx
+++ src/components/MonthBlurb.tsx
@@ -3,4 +3,5 @@ interface IProps {
     month: string
 }
 
-export const MonthBlurb = ({ month }: IProps) => "The current month is " + month + " according to TypeScript"
+export const MonthBlurb = ({ month }: IProps) => "The current month is " + month + ' according to TypeScript'
+ 
