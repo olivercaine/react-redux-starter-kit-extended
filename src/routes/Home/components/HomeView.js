f44ad0032afb3a529fd325b68252a023d8cc976e
Oliver Caine <olliecaine@gmail.com>
2021-08-25 23:30:57 +1000

    Removing @common/month-enum-ts and using named HomeView export

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,4 +1,4 @@
-import { Months } from '@common/month-enum-ts/lib/Months'
+import { Months } from '@olliecaine/months'
 import React from 'react'
 import { MonthBlurb } from '../../../components/MonthBlurb'
 import DuckImage from '../assets/Duck.jpg'
@@ -10,5 +10,3 @@ export const HomeView = () => (
     <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
   </div>
 )
-
-export default HomeView
