025fbf635ec82618eed2696bff04a1cbb00658fb
olivercaine <oliver.caine@fluentcommerce.com>
2022-03-20 19:04:55 +1100

    Merge branch 'modpack/latest' into update/storybook_update-from-modpacklatest
    # Conflicts:
    #	package-lock.json

--- src/connectors/ApiConnector.ts
+++ src/connectors/ApiConnector.ts
@@ -1,10 +1,10 @@
 import Urls from '../routes/Urls';
 
-async function fetchAsync (url: string) {
+async function fetchAsync(url: string) {
   const response = await fetch(url);
   return response.json();
 }
 
-export async function createRandomNumber (delay: number): Promise<{ randomNumber: number }> {
+export async function createRandomNumber(delay: number): Promise<{ randomNumber: number }> {
   return fetchAsync(`${Urls.randomNumber}?t=${delay}`);
 }
