8e2d1891d723d98c16b113bfdc26534a8bcc98ec
Oliver Caine <olliecaine@gmail.com>
2022-05-24 18:20:36 +1000

    Adding link principles

--- src/components/Principles.stories.tsx
+++ src/components/Principles.stories.tsx
@@ -62,3 +62,10 @@ export const Forms: Story = () => <div>
   </ol>
 
 </div>
+
+export const Links: Story = () => <div>
+  <h2>Design</h2>
+  <ol>
+    <li>Link text should be descriptive, e.g. "If you want to learn more, <a href="#">read our documentation</a>" is a lot easier for users to find than "If you want to learn more, read our documentation <a href="#">here</a>"</li>
+  </ol>
+</div>
