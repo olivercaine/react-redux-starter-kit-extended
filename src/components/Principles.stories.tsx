491044ca4f69aac819ebd648f0ef1ab1f6c94b7a
Oliver Caine <olliecaine@gmail.com>
2022-05-24 18:21:03 +1000

    Lint

--- src/components/Principles.stories.tsx
+++ src/components/Principles.stories.tsx
@@ -66,6 +66,6 @@ export const Forms: Story = () => <div>
 export const Links: Story = () => <div>
   <h2>Design</h2>
   <ol>
-    <li>Link text should be descriptive, e.g. "If you want to learn more, <a href="#">read our documentation</a>" is a lot easier for users to find than "If you want to learn more, read our documentation <a href="#">here</a>"</li>
+    <li>Link text should be descriptive, e.g. "If you want to learn more, <a href='#'>read our documentation</a>" is a lot easier for users to find than "If you want to learn more, read our documentation <a href='#'>here</a>"</li>
   </ol>
 </div>
