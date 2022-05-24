f67e21a203603a3c906e74ab6d7907c30ec1d8b0
Oliver Caine <olliecaine@gmail.com>
2022-05-24 20:44:07 +1000

    Reducing link size in Links

--- src/components/Principles/Links.stories.tsx
+++ src/components/Principles/Links.stories.tsx
@@ -14,6 +14,11 @@ export default {
 export const Links: Story = () => <div>
   <h2>Design</h2>
   <ol>
-    <li>Link text should be descriptive, e.g. "If you want to learn more, <a href='#'>read our documentation</a>" is a lot easier for users to find than "If you want to learn more, read our documentation <a href='#'>here</a>"</li>
+    <li>Link text should be descriptive so that users can quickly find what they're looking for.
+      <ol>
+        <li>Ok: "If you want to learn more, read our documentation <a href='#'>here</a>"</li>
+        <li>Better: "If you want to learn more, <a href='#'>read our documentation</a>"</li>
+      </ol>
+    </li>
   </ol>
 </div>
