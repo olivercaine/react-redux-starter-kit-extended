63789a3ffbf7c3bf88322df3f2afaa4f078858b4
Oliver Caine <olliecaine@gmail.com>
2022-07-12 02:11:07 +1000

    Auto lint

--- .github/workflows/code-lint.sh
+++ .github/workflows/code-lint.sh
@@ -2,6 +2,8 @@ npm run lint:fix
 
 if [[ `git status --porcelain` ]]; then
   echo 'Changes'
+  git config --global user.email "olliecaine@gmail.com"
+  git config --global user.name "Oliver Caine"
   git add -A && git commit -m "Performing auto-lint fix"
   git push
   exit 1
