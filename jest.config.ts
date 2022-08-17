d115eea84781169b462f5bb7c14235dd9ce94049
Oliver Caine <olliecaine@gmail.com>
2022-08-17 11:46:23 +1000

    Adding transform

--- jest.config.ts
+++ jest.config.ts
@@ -174,8 +174,7 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
