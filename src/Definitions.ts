a307b46fbcef76568d89167f20b553fad4a6f21f
Oliver Caine <olliecaine@gmail.com>
2022-08-26 17:02:51 +1000

    Group health check output and kill other processes if one fails - reducing feedback time

--- src/Definitions.ts
+++ src/Definitions.ts
@@ -1,10 +1,10 @@
-import { IAuthState } from './reducers/AuthReducer'
+import { IAuthState } from "./reducers/AuthReducer";
 // As definitions are likely to change during the early stages of development it's recommended to keep all
 // all definitions - models, interfaces etc - into this file until it gets too big to manage.
 
 export interface IRootState {
   counter: ICounter;
-  signInForm: IAuthState
+  signInForm?: IAuthState
   // canonicalData: ICanonicalData;
 }
 
