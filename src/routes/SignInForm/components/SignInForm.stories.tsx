2d6b24efcd894f524dc6c3c88a03a6c0e6e7fd41
Oliver Caine <olliecaine@gmail.com>
2022-08-29 04:35:47 +1000

    Enabling more interactions (#129)
    * Enabling more interaction tests
    
    * Lint
    
    * Submit button disables
    
    * Server validation failed story
    
    * Run integration on push

--- src/routes/SignInForm/components/SignInForm.stories.tsx
+++ src/routes/SignInForm/components/SignInForm.stories.tsx
@@ -1,7 +1,7 @@
 import { action } from '@storybook/addon-actions'
 import { expect } from '@storybook/jest'
 import { ComponentMeta } from '@storybook/react'
-import { within } from '@storybook/testing-library'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
 import { storyTemplate } from '../../../../.storybook/helpers'
 import { IProps, SignInForm } from './SignInForm'
 
@@ -26,48 +26,51 @@ export const WithInitialValues = template({
   ...requiredProps,
   initialValues: {
     email: 'olliecaine@gmail.com',
+    password: 'mypass'
   }
 })
 
 export const WithProp = template({
   ...requiredProps,
-  customProp: 'A custom prop'
+  customProp: 'A custom prop',
 })
+WithProp.play = async ({ canvasElement }) => {
+  const canvas = await within(canvasElement)
+  await waitFor(() => expect(canvas.getByText('A custom prop')).toBeInTheDocument())
+}
 
 export const WithGeneralErrors = template({
   ...requiredProps,
   generalErrors: ['Server validation failed']
 })
+WithGeneralErrors.play = async ({ canvasElement }) => {
+  const canvas = await within(canvasElement)
+  await waitFor(() => expect(canvas.getByText('Server validation failed')).toBeInTheDocument())
+}
 
 export const RequiresEmail = template({ ...requiredProps })
-// RequiresEmail.play = async ({ canvasElement }) => {
-//   const canvas = await within(canvasElement)
-//   await userEvent.click(canvas.getByRole('button'))
-//   await expect(canvas.getByText('Email is required')).toBeInTheDocument()
-// }
+RequiresEmail.play = async ({ canvasElement }) => {
+  const canvas = await within(canvasElement)
+  await userEvent.click(canvas.getByRole('button'))
+  await waitFor(() => expect(canvas.getByText('Email is required')).toBeInTheDocument())
+}
 
 export const PasswordIsRequired = template({ ...requiredProps })
-// PasswordIsRequired.play = async ({ canvasElement }) => {
-//   const canvas = within(canvasElement)
-//   await userEvent.type(canvas.getByTestId('email'), 'me@mail.com')
-//   await userEvent.click(canvas.getByRole('button'))
-//   await expect(canvas.getByText('Password is required!')).toBeInTheDocument()
-// }
+PasswordIsRequired.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('email'), 'me@mail.com')
+  await userEvent.click(canvas.getByRole('button'))
+  await waitFor(() => expect(canvas.getByText('Password is required')).toBeInTheDocument())
+}
 
-export const Submitting = template({
+export const SubmitButtonDisables = template({
   ...requiredProps,
-  initialValues: {
-    email: 'olliecaine@gmail.com',
-    password: 'pass123'
-  },
   submitting: true
 })
-// Submitting.play = async ({ canvasElement }) => {
-//   const canvas = within(canvasElement)
-//   await userEvent.type(canvas.getByTestId('email'), 'michael@chromatic.com')
-//   await userEvent.type(canvas.getByTestId('password'), 'pass')
-//   await userEvent.click(canvas.getByRole('button'))
-//   await canvas.getByTestId('password').blur()
-//   console.log("Olloe", canvas.getByText('Password needs at least'))
-//   await expect(canvas.getByText('Password needs at least')).toBeInTheDocument()
-// }
+SubmitButtonDisables.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('email'), 'me@email.com')
+  await userEvent.type(canvas.getByTestId('password'), 'myMass1*')
+  await userEvent.click(canvas.getByRole('button'))
+  await expect(canvas.getByRole('button')).toHaveProperty('disabled', true)
+}
