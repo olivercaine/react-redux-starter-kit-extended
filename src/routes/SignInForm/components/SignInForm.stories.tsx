6988490dc09a5015a27b00acf32a0eec79ff6d26
Oliver Caine <olliecaine@gmail.com>
2022-08-29 12:29:54 +1000

    Story action test (#131)
    * Adding callback test
    
    * Lint
    
    * Using jest.fn() for storybook callback test

--- src/routes/SignInForm/components/SignInForm.stories.tsx
+++ src/routes/SignInForm/components/SignInForm.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from '@storybook/addon-actions'
-import { expect } from '@storybook/jest'
+import { expect, jest } from '@storybook/jest'
 import { ComponentMeta } from '@storybook/react'
 import { userEvent, waitFor, within } from '@storybook/testing-library'
 import { storyTemplate } from '../../../../.storybook/helpers'
@@ -69,8 +69,26 @@ export const SubmitButtonDisables = template({
 })
 SubmitButtonDisables.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
-  await userEvent.type(canvas.getByTestId('email'), 'me@email.com')
-  await userEvent.type(canvas.getByTestId('password'), 'myMass1*')
-  await userEvent.click(canvas.getByRole('button'))
   await expect(canvas.getByRole('button')).toHaveProperty('disabled', true)
+}
+
+export const TriggersCallback = template({
+  ...requiredProps,
+  submitting: false,
+  onSubmit: jest.fn()
+})
+TriggersCallback.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('email'), 'me@email.com')
+  await userEvent.type(canvas.getByTestId('password'), 'myPass1*')
+  await userEvent.click(canvas.getByRole('button'))
+  await waitFor(() => {
+    expect(args.onSubmit).toHaveBeenCalledTimes(1)
+    expect(args.onSubmit).toHaveBeenCalledWith(
+      {
+        email: 'me@email.com',
+        password: 'myPass1*'
+      }
+    )
+  })
 }
