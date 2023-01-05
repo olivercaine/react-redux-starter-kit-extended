77f3f9f26d31148a71f94cf5d6025f76deef8b96
Oliver Caine <olliecaine@gmail.com>
2023-01-05 12:16:36 +0000

    Performing auto-lint fix

--- src/routes/SignInForm/index.ts
+++ src/routes/SignInForm/index.ts
@@ -4,7 +4,7 @@ export default (store) => ({
   path: 'form',
   /*  Async getComponent is only invoked when route matches   */
   // @ts-expect-error
-  getComponent(nextState, cb) {
+  getComponent (nextState, cb) {
     /*  Webpack - use 'require.ensure' to create a split point
     and embed an async module loader (jsonp) when bundling   */
     // @ts-expect-error
