076ad94b82148e43bab3f11ad6b34f4bc65c8692
Oliver Caine <olliecaine@gmail.com>
2023-04-30 20:25:17 +1000

    Fixing imports

--- src/routes/SignInForm/index.ts
+++ src/routes/SignInForm/index.ts
@@ -3,14 +3,14 @@ import { injectReducer } from '../../store/reducers'
 export default (store) => ({
   path: 'form',
   /*  Async getComponent is only invoked when route matches   */
-  getComponent (nextState, cb) {
+  getComponent(nextState, cb) {
     /*  Webpack - use 'require.ensure' to create a split point
     and embed an async module loader (jsonp) when bundling   */
     require.ensure([], (require) => {
       /*  Webpack - use require callback to define
           dependencies for bundling   */
       const Counter = require('./containers/FormContainer').default
-      const reducer = require('../../reducers/AuthReducer').authReducer
+      const reducer = require('../../store/reducers/AuthReducer').authReducer
 
       /*  Add the reducer to the store on key 'counter'  */
       injectReducer(store, { key: 'signInForm', reducer })
