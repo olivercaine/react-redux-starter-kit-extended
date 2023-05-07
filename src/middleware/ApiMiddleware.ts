9b48b9f7988a083f5cf1a183715cf81fcca98a47
Oliver Caine <olliecaine@gmail.com>
2023-05-07 20:04:33 +1000

    Bump @olliecaine/reducers to 1.0.17

--- src/middleware/ApiMiddleware.ts
+++ src/middleware/ApiMiddleware.ts
@@ -1,17 +1,17 @@
 import { IHttpResponse } from '@olliecaine/fetch'
 import { Dispatch, Middleware } from 'redux'
 import { LoginResponse, login } from '../connectors/ApiConnector'
-import { AuthAction, AuthActions } from '../state/reducers/AuthReducer'
+import { ActionTypes as AuthActionTypes, authReducer } from '../state/reducers/AuthReducer'
 import { CounterActions } from './../routes/Counter/modules/counter'
 
 // Receives all actions but only processes ones defined below before they reach the store's reducer.
 export const apiMiddleware: Middleware = (store) => (next: Dispatch<any>) => (action) => {
   switch (action.type) {
-    case AuthAction.SHOULD_SIGN_IN:
+    case AuthActionTypes.SHOULD_SIGN_IN:
       login()
         .then((response: IHttpResponse<LoginResponse>) => {
           store.dispatch(
-            AuthActions.didSignIn({
+            authReducer.actions.didSignIn({
               submitting: false,
               token: response.parsedBody?.token,
               generalErrors: []
@@ -20,7 +20,7 @@ export const apiMiddleware: Middleware = (store) => (next: Dispatch<any>) => (ac
         })
         .catch(() => {
           store.dispatch(
-            AuthActions.didSignIn({
+            authReducer.actions.didSignIn({
               token: undefined,
               submitting: false,
               generalErrors: [
