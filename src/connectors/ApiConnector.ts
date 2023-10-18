18a93aa1a78786850d2532c131a17a9cdbfb0349
Oliver Caine <olliecaine@gmail.com>
2023-10-18 16:54:21 +1100

    Updating @olliecaine/eslint

--- src/connectors/ApiConnector.ts
+++ src/connectors/ApiConnector.ts
@@ -2,7 +2,7 @@ import { IHttpResponse, post } from '@olliecaine/fetch'
 
 export interface LoginResponse {
   token: string
-};
+}
 export const authApi = '/mock'
 export const login = async (creds = undefined): Promise<IHttpResponse<LoginResponse>> =>
   post<LoginResponse>(authApi, creds)
