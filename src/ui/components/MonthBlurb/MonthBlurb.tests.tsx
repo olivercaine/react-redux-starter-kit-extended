13c690ce3e6dc6ad7b23c25a88bbef2412ce23fe
Oliver Caine <olliecaine@gmail.com>
2024-01-11 01:51:32 +1100

    Fixing test

--- src/ui/components/MonthBlurb/MonthBlurb.tests.tsx
+++ src/ui/components/MonthBlurb/MonthBlurb.tests.tsx
@@ -1,21 +1,23 @@
 
+import { expect } from '@storybook/jest'
 import '@testing-library/jest-dom'
 import '@testing-library/jest-dom/extend-expect'
 // import { render /*, screen*/ } from '@testing-library/react'
 // import React from 'react'
 // import { MonthBlurb } from './MonthBlurb'
 
-describe.skip('Month is Feb', () => {
+describe('Month is Feb', () => {
   describe('Building blurb text', () => {
-    // it('Returns which month it is', () => {
-    //   // Arrange
-    //   const month = 'feb'
+    it('Returns which month it is', () => {
+      // Arrange
+      const month = 'feb'
 
-    //   // Act
-    //   render(<MonthBlurb month={month} />)
+      // Act
+      // render(<MonthBlurb month={month} />)
 
-    //   // Assert
-    //   // expect(screen.getByText(/Hello, World!/i)).toBeInTheDocument()
-    // })
+      // Assert
+      // expect(screen.getByText(/Hello, World!/i)).toBeInTheDocument()
+      expect(month).not.toBeNull()
+    })
   })
 })
